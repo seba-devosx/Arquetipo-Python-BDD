02e93ce5af5e8bb79e51f1753548e2cc887a38a7
Sebastian Aravena Sandoval <sebastianaravenasandoval@MacBook-Pro-de-Sebastian.local>
2023-03-28 13:46:42 -0300

    Se modifica version de seleium, de igualmanera se modifica la sintaxis para la busqueda de elementos

--- features/steps/hakatools.py
+++ features/steps/hakatools.py
@@ -1,6 +1,8 @@
 
 from site import execusercustomize
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from behave import given,when,then
 import time
 class hakatool:
@@ -15,14 +17,13 @@ class hakatool:
 
     @when(u'seleccione el apartado de hakatool')
     def step_impl(context):
-        hktool=wdriver.find_element_by_xpath("//a[contains(text(),'HakaTools')][1]")
-        hktool.click()
+        hktool=wdriver.find_element(By.XPATH,"//a[contains(text(),'HakaTools')][1]").click()
         time.sleep(5)
         
 
     @when(u'seleccione formulario')
     def step_impl(context):
-        hktoolform= wdriver.find_element_by_xpath("/html/body/app-root/app-cardsmenu/div[2]/div/div/div[2]/div/div")
+        hktoolform= wdriver.find_element(By.XPATH,"/html/body/app-root/app-cardsmenu/div[2]/div/div/div[2]/div/div")
         wdriver.execute_script("arguments[0].scrollIntoView(true);",hktoolform)
         hktoolform.click()
         #time.sleep(5)
@@ -32,19 +33,19 @@ class hakatool:
     @when(u'ingrese el nombre {nombre} ,{correo} ,{dir1} ,{dir2}')
     def step_impl(context,nombre,correo,dir1,dir2):
         
-        hktoolname=wdriver.find_element_by_xpath("/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/div[1]/input")
+        hktoolname=wdriver.find_element(By.XPATH,"/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/div[1]/input")
         hktoolname.send_keys(nombre)
 
-        hktoolcorreo=wdriver.find_element_by_xpath("/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/div[2]/input")
+        hktoolcorreo=wdriver.find_element(By.XPATH,"/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/div[2]/input")
         hktoolcorreo.send_keys(correo)
 
-        hktooldir1=wdriver.find_element_by_xpath("/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/div[3]/input")
+        hktooldir1=wdriver.find_element(By.XPATH,"/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/div[3]/input")
         hktooldir1.send_keys(dir1)
 
-        hktooldir2=wdriver.find_element_by_xpath("/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/div[4]/input")
+        hktooldir2=wdriver.find_element(By.XPATH,"/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/div[4]/input")
         hktooldir2.send_keys(dir2)
         
-        hktoolbt=wdriver.find_element_by_xpath("/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/button")
+        hktoolbt=wdriver.find_element(By.XPATH,"/html/body/app-root/app-forms/div[2]/div[2]/div[1]/app-basic-form/div/div[2]/form/div/button")
         hktoolbt.click()
         wdriver.close()
 
