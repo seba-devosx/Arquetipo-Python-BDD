ea147b2c66181b37f02e5bf8d2366b5049f8d2f2
sebastianaravenasandoval <sebastianignaaravenasandoval@gmail.com>
2024-07-29 15:52:04 -0400

    se modifica readme.md

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Arquetipo para la realización de testing en software
 Ejecuctar y generar datos para reporte
 
 ```
-behave -f allure_behave.formatter:AllureFormatter -o allure-2.10.0/results ./features/Flujo1.feature
+behave -f allure_behave.formatter:AllureFormatter -o helpers/allure-2.30.0/allure-result ./features/busqueda.feature
 ```
 Una vez ejecutado el comando anterior este ejecutara el feature seleccionado deplegando en navegador
 en tu pantalla e iniciara el flujo a ejecuctar, el arquetipo esta configurado para que despues de cada
 prueba este realizara la captura de cada uno de los pasos que se van ejecutando, para cuando 
 termine el proceso de ejecucion deplegue el reporte de allure en tu navegador, en el podras visualizar 
 la cantidad de step los cuales hayan pasado de manera satisfactoria la prueba y en cuales han habido fallos
+
+Ya una vez finalizado el test, el reporte se visualizara de manera automatica en tu navegeador
--- features/environment.py
+++ features/environment.py
@@ -1,51 +1,28 @@
-
-from selenium.webdriver.chrome.options import Options
-from allure_commons.types import AttachmentType
 from selenium import webdriver
-from allure_behave import hooks
 import allure
+from allure_commons.types import AttachmentType
 import os
-import time
-import random
-
-# before all
-#
 
 def before_all(context):
-   print('Before all executed')
-   driver_path = os.path.join(os.getcwd(), "selenium/webdriver/chromedriver")
-   context.driver = webdriver.Chrome(driver_path)
-   chrome_options = Options()
-   #chrome_options.add_argument("--headless")
-   chrome_options.add_argument('--start-maximized')
-   context.driver = webdriver.Chrome(chrome_options=chrome_options)
-# before every scenario
-def before_scenario(scenario, context):
-   print("before_scenario")
-   #time.sleep(10)
-# after every feature
-def after_scenario(context, scenario):
-    print("after_scenario")
+    print('Before all executed')
+    context.driver = webdriver.Chrome("helpers/selenium/webdriver/chromedriver")
 
-def before_step(context, step):
-   #allure.attach('Hello there!', name='attachment', attachment_type=allure.attachment_type.PNG)
-   if not os.path.exists("assets/screenshots/"):
-      os.makedirs("assets/screenshots/")
-      print("dierectorio creado")
-   print("dierectorio ya existe!")
-   allure.attach(context.driver.get_screenshot_as_png(), name="screenshot", attachment_type=AttachmentType.PNG)
+def before_scenario(context, scenario):
+   print('Before scenario executed')
+   
+def after_step(context,step):
+   print('After step executed')
+   try:
+         # Capturar la captura de pantalla y adjuntar a Allure
+         allure.attach(context.driver.get_screenshot_as_png(), name=f'Screensho-{step}', attachment_type=AttachmentType.JPG)
+   except Exception as e:
+         print(f"Error al tomar la captura de pantalla: {e}")
+         
+def after_feature(context, feature):
+   print('After feature executed')
+   
 
-
-def after_step(context, step):
-   print("after_step")
-   if step.status == "pass":
-        allure.attach(context.driver.get_screenshot_as_png(), name="screenshot", attachment_type=AttachmentType.PNG)
-
-
-# after all
 def after_all(context):
-   print("after_all")
-   context.driver.quit()
-   os.system("sh allure-2.10.0/bin/allure serve --clean ../results      allure-2.10.0/results")
-   
-   
+    print('After all executed')
+    #ejecuta comando para levantar el reporte de allure 
+    os.popen('helpers/allure-2.30.0/bin/allure serve helpers/allure-2.30.0/allure-result')
