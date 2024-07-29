ae192f6a004a29ff09ab2527e736003f560487a6
sebastianaravenasandoval <sebastianignaaravenasandoval@gmail.com>
2024-07-29 15:43:28 -0400

    se agrega la captura de pantalla para los resportes de allure

--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 import allure
 from allure_commons.types import AttachmentType
+import os
 
 def before_all(context):
     print('Before all executed')
@@ -24,4 +25,5 @@ def after_feature(context, feature):
 
 def after_all(context):
     print('After all executed')
-    
+    #ejecuta comando para levantar el reporte de allure 
+    os.popen('helpers/allure-2.30.0/bin/allure serve helpers/allure-2.30.0/allure-result')
